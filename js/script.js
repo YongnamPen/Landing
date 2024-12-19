18ec04f5debc3e4bc048d7ac02a772840da27ef4
Yongnam <yongnamfs@gmail.com>
2024-12-19 23:40:11 +0300

    Сверстал 4 и 7 экран

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function correctDate (date) {
+    return date <10 ? "0" + date : date;
+}
+
+function saleDate() {
+    const day = document.getElementsByClassName("time")[0];
+    const hour = document.getElementsByClassName("time")[1];
+    const minutes = document.getElementsByClassName("time")[2];
+    const seconds = document.getElementsByClassName("time")[3];
+    const finishDate = new Date(2024,11,31)
+    const currentDate = new Date()
+    const date = finishDate-currentDate
+    day.innerHTML = correctDate(Math.trunc (date/(1000 * 60 * 60 * 24)));
+    hour.innerHTML = correctDate(Math.trunc ((date%(1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    minutes.innerHTML = correctDate(Math.trunc((date%(1000 * 60 * 60)) / (1000 * 60)))
+    seconds.innerHTML = correctDate(Math.trunc((date%(1000 * 60)) / 1000))
+}
+saleDate()
+setInterval(function(){
+    saleDate();
+},1000)
