abae01cc39612a89aba1b48ac75ca577ecf87427
Yongnam <yongnamfs@gmail.com>
2024-12-21 00:12:16 +0300

    Создал две страницы рабочего таймера

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-function correctDate (date) {
-    return date <10 ? "0" + date : date;
+function correctDate(date) {
+    return date < 10 ? "0" + date : date;
 }
 
 function saleDate() {
-    const day = document.getElementsByClassName("time")[0];
-    const hour = document.getElementsByClassName("time")[1];
-    const minutes = document.getElementsByClassName("time")[2];
-    const seconds = document.getElementsByClassName("time")[3];
-    const finishDate = new Date(2024,11,31)
-    const currentDate = new Date()
-    const date = finishDate-currentDate
-    day.innerHTML = correctDate(Math.trunc (date/(1000 * 60 * 60 * 24)));
-    hour.innerHTML = correctDate(Math.trunc ((date%(1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    minutes.innerHTML = correctDate(Math.trunc((date%(1000 * 60 * 60)) / (1000 * 60)))
-    seconds.innerHTML = correctDate(Math.trunc((date%(1000 * 60)) / 1000))
+    const timerSections = document.querySelectorAll(".thirdPage");
+
+    timerSections.forEach(section => {
+        const day = section.getElementsByClassName("time")[0];
+        const hour = section.getElementsByClassName("time")[1];
+        const minutes = section.getElementsByClassName("time")[2];
+        const seconds = section.getElementsByClassName("time")[3];
+
+        const finishDate = new Date(2024, 11, 31);
+        const currentDate = new Date();
+        const date = finishDate - currentDate;
+
+        day.innerHTML = correctDate(Math.trunc(date / (1000 * 60 * 60 * 24)));
+        hour.innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        minutes.innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60)));
+        seconds.innerHTML = correctDate(Math.trunc((date % (1000 * 60)) / 1000));
+    });
 }
-saleDate()
-setInterval(function(){
+saleDate();
+setInterval(function () {
     saleDate();
-},1000)
+}, 1000);
